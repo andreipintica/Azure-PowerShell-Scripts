dcc175ca0bbff9076228df2a419643ebcea6c6ee
Andrei Pintica <andrei.pintica@gmail.com>
2021-07-05 08:25:02 +0300

    Add files via upload

---
+++ AD_Groups_Export.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+AD Groups export.
+.DESCRIPTION
+Export Active Directory Groups from the main domain.
+.EXAMPLE
+Run script and use the function.
+
+.NOTES
+Name: Local-admin-remote
+Version: 1.0.0
+Author: Andrei Pintica (@AndreiPintica)
+#>
+
+#// Start of script
+#// Get year and month for csv export file
+$DateTime = Get-Date -f "yyyy-MM"
+
+#// Set CSV file name
+$CSVFile = "C:\AD_Groups"+$DateTime+".csv"
+
+#// Create emy array for CSV data
+$CSVOutput = @()
+
+#// Get all AD groups in the domain
+$ADGroups = Get-ADGroup -Filter *
+
+#// Set progress bar variables
+$i=0
+$tot = $ADGroups.count
+
+foreach ($ADGroup in $ADGroups) {
+	#// Set up progress bar
+	$i++
+	$status = "{0:N0}" -f ($i / $tot * 100)
+	Write-Progress -Activity "Exporting AD Groups" -status "Processing Group $i of $tot : $status% Completed" -PercentComplete ($i / $tot * 100)
+
+	#// Ensure Members variable is empty
+	$Members = ""
+
+	#// Get group members which are also groups and add to string
+	$MembersArr = Get-ADGroup -filter {Name -eq $ADGroup.Name} | Get-ADGroupMember | select Name
+	if ($MembersArr) {
+		foreach ($Member in $MembersArr) {
+			$Members = $Members + "," + $Member.Name
+		}
+		$Members = $Members.Substring(1,($Members.Length) -1)
+	}
+
+	#// Set up hash table and add values
+	$HashTab = $NULL
+	$HashTab = [ordered]@{
+		"Name" = $ADGroup.Name
+		"Category" = $ADGroup.GroupCategory
+		"Scope" = $ADGroup.GroupScope
+		"Members" = $Members
+	}
+
+	#// Add hash table to CSV data array
+	$CSVOutput += New-Object PSObject -Property $HashTab
+}
+
+#// Export to CSV files
+$CSVOutput | Sort-Object Name | Export-Csv $CSVFile -NoTypeInformation
+
+#// End of script
