4f4042dd20ee95d4b2557d5075f8f49ff901a95b
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 07:04:29 +0200

    Create Set-Az-VM-Size.ps1

---
+++ Compute/Azure VM/Set-Az-VM-Size.ps1
@@ -0,0 +1,51 @@
+<#
+    .SYNOPSIS
+    Sets virtual machine size
+    
+    .COMPONENT
+    Requires Module Az
+    
+   .Parameter Name
+    Specifies the name of the virtual machine
+    
+    .Parameter Size
+     Specifies the new size of the virtual machine
+        
+    .Parameter ResourceGroupName
+    Specifies the name of the resource group of the virtual machine
+#>
+
+param( 
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroupName,
+    [Parameter(Mandatory = $true)]
+    [string]$Name,
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('Standard_DS1_v2','Standard_DS2_v2','Standard_DS3_v2','Standard_DS4_v2','Standard_DS5_v2')]
+    [string]$Size
+)
+
+Import-Module Az.Compute
+
+try{
+    $vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $Name -ErrorAction Stop
+    $vm.HardwareProfile.VmSize = $Size
+
+    Stop-AzVM -ResourceGroupName $ResourceGroupName -Name $Name -Force -ErrorAction Stop
+    Update-AzVM -VM $vm -ResourceGroupName $ResourceGroupName -ErrorAction Stop
+    Start-AzVM -ResourceGroupName $ResourceGroupName -Name $Name -ErrorAction Stop
+
+    $ret = Get-AzVMSize -ResourceGroupName $ResourceGroupName -VMName $Name -ErrorAction Stop
+
+    if($SRXEnv) {
+        $SRXEnv.ResultMessage = $ret 
+    }
+    else{
+        Write-Output $ret
+    }
+}
+catch{
+    throw
+}
+finally{
+}
